3bb43866ca09b58442220f12876bca06f6dc6a3f
Ericdiii <70551470+Ericdiii@users.noreply.github.com>
2021-04-02 22:19:11 +1100

    Update network.py

--- network.py
+++ network.py
@@ -3,30 +3,38 @@ import torch.nn as nn
 import torch.utils.data
 import torch.nn.functional as F
 
+# Create a new network class and inherit the nn.Module parent class of PyTorch
+class Net(nn.Module):                                      
+    def __init__(self):                                     # Construct function to set the network layer
+        super(Net, self).__init__()                         # Standard statement
+        
+        # First convolutional layer: 3 input channels, 16 output channels, 3×3 convolution kernel, 1 padding, other parameters are default
+        self.conv1 = torch.nn.Conv2d(3, 16, 3, padding=1)
+        # Second convolutional layer: 16 input channels, 16 output channels, 3×3 convolution kernel, 1 padding, other parameters are default
+        self.conv2 = torch.nn.Conv2d(16, 16, 3, padding=1)
 
-class Net(nn.Module):                                       # 新建一个网络类，就是需要搭建的网络，必须继承PyTorch的nn.Module父类
-    def __init__(self):                                     # 构造函数，用于设定网络层
-        super(Net, self).__init__()                         # 标准语句
-        self.conv1 = torch.nn.Conv2d(3, 16, 3, padding=1)   # 第一个卷积层，输入通道数3，输出通道数16，卷积核大小3×3，padding大小1，其他参数默认
-        self.conv2 = torch.nn.Conv2d(16, 16, 3, padding=1)  # 第二个卷积层，输入通道数16，输出通道数16，卷积核大小3×3，padding大小1，其他参数默认
+        # First full connected layer: Linear connection, 50×50×16 input nodes, 128 output nodes
+        self.fc1 = nn.Linear(50*50*16, 128) 
+        # Second full connected layer: Linear connection, 128 input nodes, 64 output nodes
+        self.fc2 = nn.Linear(128, 64)
+        # Third full connected layer: Linear connection, 64 input nodes, 2 output nodes
+        self.fc3 = nn.Linear(64, 2)
+    
+    # Rewrite the forward method of the parent class: Forward calculation, obtain the output after the network receive the input data
+    def forward(self, x):
+        
+        x = self.conv1(x)                   # ConV1
+        x = F.relu(x)                       # Process the result through the ReLU activation function
+        x = F.max_pool2d(x, 2)              # 2×2 Max pooling
 
-        self.fc1 = nn.Linear(50*50*16, 128)                 # 第一个全连层，线性连接，输入节点数50×50×16，输出节点数128
-        self.fc2 = nn.Linear(128, 64)                       # 第二个全连层，线性连接，输入节点数128，输出节点数64
-        self.fc3 = nn.Linear(64, 2)                         # 第三个全连层，线性连接，输入节点数64，输出节点数2
+        x = self.conv2(x)                   # ConV2
+        x = F.relu(x)                       # Process the result through the ReLU activation function
+        x = F.max_pool2d(x, 2)              # 2×2 Max pooling
 
-    def forward(self, x):                   # 重写父类forward方法，即前向计算，通过该方法获取网络输入数据后的输出值
-        x = self.conv1(x)                   # 第一次卷积
-        x = F.relu(x)                       # 第一次卷积结果经过ReLU激活函数处理
-        x = F.max_pool2d(x, 2)              # 第一次池化，池化大小2×2，方式Max pooling
-
-        x = self.conv2(x)                   # 第二次卷积
-        x = F.relu(x)                       # 第二次卷积结果经过ReLU激活函数处理
-        x = F.max_pool2d(x, 2)              # 第二次池化，池化大小2×2，方式Max pooling
-
-        x = x.view(x.size()[0], -1)         # 由于全连层输入的是一维张量，因此需要对输入的[50×50×16]格式数据排列成[40000×1]形式
-        x = F.relu(self.fc1(x))             # 第一次全连，ReLU激活
-        x = F.relu(self.fc2(x))             # 第二次全连，ReLU激活
-        y = self.fc3(x)                     # 第三次激活，ReLU激活
+        x = x.view(x.size()[0], -1)         # One-dimensional tensor fully connected layer: arrange the [50×50×16] input data into [40000×1]
+        x = F.relu(self.fc1(x))             # FC1 activated by ReLu
+        x = F.relu(self.fc2(x))             # FC2 activated by ReLu
+        y = self.fc3(x)                     # FC3
 
         return y
 
